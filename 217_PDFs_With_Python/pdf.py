86d0901f3328a0df8aac4514684267839ab596bc
Igor Souza <igorjs.souza@gmail.com>
2022-10-25 00:20:12 -0300

    cont. aula 212: rotação + save do arquivo rotacionado

--- 217_PDFs_With_Python/pdf.py
+++ 217_PDFs_With_Python/pdf.py
@@ -3,6 +3,8 @@ with open('dummy.pdf', 'rb') as file:
 	reader = PyPDF2.PdfFileReader(file)
 	# pega apenas a pagina 1 para preparar para rotacionar
 	page = reader.getPage(0)
-	print(reader.numPages)
-	print(reader.getPage(0))
-	print(page.rotateCounterClockwise(90))
+	page.rotateCounterClockwise(90)
+	writer = PyPDF2.PdfFileWriter()
+	writer.addPage(page)
+	with open('tilt.pdf', 'wb') as new_file:
+		writer.write(new_file)
