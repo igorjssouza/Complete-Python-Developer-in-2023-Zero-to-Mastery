d4f38f18e0a34f4f4f7b5db031d4791b866add2b
igorjssouza <igorjs.souza@gmail.com>
2022-12-10 00:44:13 -0300

    commit from kyrzi's computer

--- OOP/OOP.py
+++ OOP/OOP.py
@@ -1,7 +1,7 @@
 class PlayerCharacter:
-	# Static class attribute
-	membership = True
-    def __init__(self, name, age):
+    # Static class
+    membership = True
+    def __init__(self,name,age):
         self.name = name
         self.age = age
 
@@ -9,10 +9,14 @@ class PlayerCharacter:
         print("run")
         return 'done'
 
+    def shout(self):
+        return f'My name is {self.name}'
+
 
 player1 = PlayerCharacter("Cindy", 33)
 player2 = PlayerCharacter("Tom", 40)
 
 player2.attack = 50
 
-print(player2.attack)
+print(player1.shout())
+print(player2.shout())
