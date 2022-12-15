fe8317999ef8568935087b3c8f7dd238106f6adc
Igor Souza <igorjs.souza@gmail.com>
2022-12-15 11:13:10 -0300

    Private Varible OOP

--- OOP/OOP.py
+++ OOP/OOP.py
@@ -1,19 +1,15 @@
-#Given the below class:
-class Cat:
-    species = 'mammal'
+class PlayerCharacter:
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        self._name = name
+        self._age = age
 
-# 1 Instantiate the Cat object with 3 cats
-cat1 = Cat("Tom", 3)
-cat2 = Cat("Newton", 6)
-cat3 = Cat("Garfield", 4)
+    def run(self):
+        print("run")
 
-# 2 Create a function that finds the oldest cat
-def oldestCat():
-    cats = [cat1.age,cat2.age,cat3.age]
-    return max(cats)
+    def speak(self):
+        print(f'my name is {self._name}, and I am {self._age} years old')
 
-# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
-print(f'The oldest cat is {oldestCat()} years old.')
+player1 = PlayerCharacter('Andrei', 100)
+
+
+print(player1.speak())
