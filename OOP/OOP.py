e8e8a9a16fd5dbc08f6b488bac56e37b0be2e856
Igor Souza <igorjs.souza@gmail.com>
2022-12-25 20:05:48 -0300

    exercicios dos gatinhos

--- OOP/OOP.py
+++ OOP/OOP.py
@@ -1,15 +1,38 @@
-class PlayerCharacter:
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
     def __init__(self, name, age):
-        self._name = name
-        self._age = age
+        self.name = name
+        self.age = age
 
-    def run(self):
-        print("run")
+    def walk(self):
+        return f'{self.name} is just walking around'
 
-    def speak(self):
-        print(f'my name is {self._name}, and I am {self._age} years old')
+class charCat(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
-player1 = PlayerCharacter('Andrei', 100)
+
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+cat1 = charCat("Simon", 4)
+cat2 = charCat("Sally", 3)
+cat3 = charCat("Garfield", 5)
+my_cats = [charCat("Simon", 4), cat2, cat3]
 
 
-print(player1.speak())
+#3 Instantiate the Pet class with all your cats use variable my_pets
+
+my_pets = Pets(my_cats)
+
+#4 Output all of the cats walking using the my_pets instance
+
+my_pets.walk()
